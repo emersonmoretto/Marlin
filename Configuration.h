a92a8a72c4ffa6d697349917c9df2045a94def91
Emerson Moretto <emerson@moretto.eng.br>
2014-10-15 00:06:11 -0300

    ajuste para 5.6 no z-offset do probe.. 5.3 ficou alto

--- Configuration.h
+++ Configuration.h
@@ -437,7 +437,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
   // these are the offsets to the probe relative to the extruder tip (Hotend - Probe)
   #define X_PROBE_OFFSET_FROM_EXTRUDER 14
   #define Y_PROBE_OFFSET_FROM_EXTRUDER 94
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER -5.3
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -5.6
 
   #define Z_RAISE_BEFORE_HOMING 7       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
