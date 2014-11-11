ddbd7de35e9bec655480e914b98fc73550948135
Emerson Moretto <emoretto@usp.br>
2014-11-11 15:15:33 -0200

    update do z-probe

--- Configuration.h
+++ Configuration.h
@@ -436,14 +436,13 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
   // these are the offsets to the probe relative to the extruder tip (Hotend - Probe)
   #define X_PROBE_OFFSET_FROM_EXTRUDER 14
   #define Y_PROBE_OFFSET_FROM_EXTRUDER 94
-
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER -5.3
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -5.06
 
   #define Z_RAISE_BEFORE_HOMING 15       // (in mm) Raise Z before homing (G28) for Probe Clearance.
-                                       // Be sure you have this distance over your Z_MAX_POS in case
+                                        // Be sure you have this distance over your Z_MAX_POS in case
 
   #define XY_TRAVEL_SPEED 4000         // X and Y axis travel speed between probes, in mm/min
-
+s
   #define Z_RAISE_BEFORE_PROBING 15    //How much the extruder will be raised before traveling to the first probing point.
   #define Z_RAISE_BETWEEN_PROBINGS 5  //How much the extruder will be raised when traveling from between next probing points
 
